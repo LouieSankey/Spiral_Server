04c68216d9a62b42b62df1eee4f9e338565e0444
louissankey <lmsankey@ucdavis.edu>
2020-10-14 16:51:01 -0700

    preparing for deploy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
 PORT: process.env.PORT || 8000,
 NODE_ENV: process.env.NODE_ENV || 'development',
 DATABASE_URL: process.env.DATBASE_URL || 'postgresql://postgres@localhost/spiral',
-TEST_DATABASE_URL: process.env.DATBASE_URL || 'postgresql://postgres@localhost/spiral_test'
+TEST_DATABASE_URL: process.env.TEST_DATBASE_URL || 'postgresql://postgres@localhost/spiral_test'
 
 }
