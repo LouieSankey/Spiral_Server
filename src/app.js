b390166784e3a15a5164c14c955588ae945fd87b
louissankey <lmsankey@ucdavis.edu>
2020-10-15 18:13:21 -0700

    deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
@@ -14,20 +14,20 @@ const authRouter = require('./auth/auth-router')
 
 const app = express()
 
-
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+app.use(cors())
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
 
-app.use('/account', accountRouter)
-app.use('/project', projectRouter)
-app.use('/task', taskRouter)
-app.use('/pref', prefsRouter)
-app.use('/api/auth', authRouter)
+
+app.use('account', accountRouter)
+app.use('project', projectRouter)
+app.use('task', taskRouter)
+app.use('pref', prefsRouter)
+app.use('api/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
