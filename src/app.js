47873597c46fec60fee3aa0baff8a277d63787bf
louissankey <lmsankey@ucdavis.edu>
2020-10-20 12:58:03 -0700

    deploy

--- src/app.js
+++ src/app.js
@@ -35,17 +35,15 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    console.error(error)
-    console.log("hello")
-    
+   
+
         if (NODE_ENV === 'production') {
-            
             response = { error: { message: 'server error' } }
         } else {
             console.error(error)
             response = { message: error.message, error }
         }
-    res.status(500).json(response)
+    res.status(500).json({"hello": "world"})
     })
 
 module.exports = app
